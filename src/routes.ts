f86a44e0a37261d8a8d015fd0f30dd8f0f3a317d
Abner Persio <binhopersio@gmail.com>
2025-11-01 15:23:21 -0300

    feat: ud stack

--- src/routes.ts
+++ src/routes.ts
@@ -1,40 +1,45 @@
 export type Route = {
   route: string;
   fnPath: string;
-  method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "ANY";
-  authorizer?: "jwt";
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE' | 'PATCH' | 'ANY';
+  private: boolean;
 };
 
 export const ROUTES: Route[] = [
   {
-    fnPath: "auth/sign-up",
-    method: "POST",
-    route: "/sign-up",
+    fnPath: 'auth/sign-up',
+    method: 'POST',
+    route: '/sign-up',
+    private: false,
   },
   {
-    fnPath: "auth/sign-in",
-    method: "POST",
-    route: "/sign-in",
+    fnPath: 'auth/sign-in',
+    method: 'POST',
+    route: '/sign-in',
+    private: false,
   },
   {
-    fnPath: "auth/refresh-token",
-    method: "POST",
-    route: "/refresh-token",
+    fnPath: 'auth/refresh-token',
+    method: 'POST',
+    route: '/refresh-token',
+    private: false,
   },
   {
-    fnPath: "auth/forgot-password",
-    method: "POST",
-    route: "/forgot-password",
+    fnPath: 'auth/forgot-password',
+    method: 'POST',
+    route: '/forgot-password',
+    private: false,
   },
   {
-    fnPath: "auth/reset-password",
-    method: "POST",
-    route: "/reset-password",
+    fnPath: 'auth/reset-password',
+    method: 'POST',
+    route: '/reset-password',
+    private: false,
   },
   {
-    fnPath: "meal/create-meal",
-    method: "POST",
-    route: "/meal",
-    authorizer: "jwt",
+    fnPath: 'meal/create-meal',
+    method: 'POST',
+    route: '/meal',
+    private: true,
   },
 ];
